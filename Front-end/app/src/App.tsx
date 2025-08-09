86cd755e12bb699d359666e40b98e7eba45dda5a
Juan-0428 <juandavid.mazuera@utp.edu.co>
2025-08-09 12:53:54 -0500

    Renovacion primordial del repositorio

---
+++ Front-end/app/src/App.tsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+import {Route } from 'react-router-dom';
+import Initialization from './Init';
+import NotFound from './NotFound';
+import Footer from "@components/Footer"
+import { useLocation } from 'react-router-dom';
+import { Suspense, useEffect, useRef } from 'react';
+import Forms from 'forms';
+
+
+function Dinamic_Content(){
+  const location = useLocation().pathname;
+  const refFooter = useRef<HTMLLabelElement>(null);
+  useEffect(()=>{
+    if (refFooter.current){
+      refFooter.current.textContent = location.toString();
+    }
+  },[location])
+
+  return (
+    <>
+    <Suspense fallback={<div>...Cargando</div>}>
+      <Routes>
+        
+          <Route path='/' element={<Initialization/>} />
+          <Route path='*'errorElement={<NotFound/>} element= {<NotFound/>}/>
+          <Route path='/form:mode' element= {<Forms/>}/>
+
+      </Routes>
+    </Suspense>
+    <Footer  resource= {location} ref={refFooter}></Footer>
+    </>
+  )
+
+}
+export default function App(){
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Dinamic_Content></Dinamic_Content> 
+      </BrowserRouter>
+    </>
+  )
+}
---
+++ Front-end/app/src/Init.tsx
@@ -0,0 +1,17 @@
+
+export default function Initialization(){
+
+    return (
+        <div>
+            <section>
+                <div className="header">
+                    <img src="" alt="" />
+                    <h1>
+                        header
+                    </h1>
+                    
+                </div>
+            </section>
+        </div>
+    )
+}
